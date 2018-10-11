344c6d4f467a31d515d494432279f96c9bb8da53
Enmanuel Toribio <enmanuel@torib.io>
2018-10-11 12:45:04 -0400

    Added first class code

---
+++ Clase1Ejercicio1/Clase1Ejercicio1/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clase1Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var age = (DateTime.UtcNow.Year - dtpBirthDay.Date.Year);
+            lblAge.Text = $"Tienes {age} anios de edad";
+
+        }
+    }
+}
